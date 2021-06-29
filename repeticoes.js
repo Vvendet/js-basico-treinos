be1084400743aba24c4f2b82ddca8aa950659b9d
Julio Cesar Froes de Oliveira <froees147@gmail.com>
2021-06-29 17:36:47 -0300

    segundo commit

--- repeticoes.js
+++ repeticoes.js
@@ -2,3 +2,11 @@ var nomes = ["jose","joao","julio","julia","juliana"]
 for (var i = 0; i < nomes.length; i++){
     document.write(nomes[i]+" ")
 }
+var contador = 0
+while (contador < 20){
+    console.log("Este é um comando feito por while")
+    console.log("Fui obrigado a fazer isso por não ter feito")
+    console.log("Anotações durante a aula, então estou aqui!")
+    console.log("O contador vai chegar até 20 para parar de fazer isso, então eu vou acelerar o processo acresentando um numero maior que um ao valor dele, confia")
+    contador += 5
+}
