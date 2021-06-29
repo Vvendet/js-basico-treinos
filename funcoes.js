e78ed53ed4adc6a444b2bfec6c626333310a5d8b
Julio Cesar Froes de Oliveira <froees147@gmail.com>
2021-06-29 17:54:56 -0300

    atualizando func

---
+++ funcoes.js
@@ -0,0 +1,11 @@
+nome = prompt("Digite um nome: ")
+
+function sayHelo(name){
+    document.write("Olá" + " " +name)
+}
+sayHelo(nome)
+function sumSomar(a,b){
+    return a + b
+}
+var soma = sumSomar(2,5)
+document.write(soma)
